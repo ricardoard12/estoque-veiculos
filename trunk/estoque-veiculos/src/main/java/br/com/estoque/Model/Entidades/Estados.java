4f285918e90b918a65f9a4f83a96350f0e14dc76
Rogerio.koglin@gmail.com <Rogerio.koglin@gmail.com@b3fc6d30-5574-8154-f2e4-00a9b778fb8a>
2014-07-02 00:45:28 +0000

    Ajustes finais nas entidades.

--- trunk/estoque-veiculos/src/main/java/br/com/estoque/Model/Entidades/Estados.java
+++ trunk/estoque-veiculos/src/main/java/br/com/estoque/Model/Entidades/Estados.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.estoque.model.Entidades;
+package br.com.estoque.Model.Entidades;
 
 import java.io.Serializable;
 import java.util.List;
